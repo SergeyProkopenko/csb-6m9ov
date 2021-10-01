d94ed035da2fb1d5be436b582b351b7fb869d3a3
Code Sandbox <hello@codesandbox.io>
2021-10-01 13:44:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.99d4b7e016d40b3d3f4f44a55ddf8ac2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca4798c6de5b9d623b93d637c82d3d1",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/index.html"
+  },
+  {
+    "revision": "8e12eefde8f1c95e2c03",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/css/2.dfd58508.chunk.css"
+  },
+  {
+    "revision": "2eea0cf90c5c449b836f",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/css/main.4f11939e.chunk.css"
+  },
+  {
+    "revision": "8e12eefde8f1c95e2c03",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/js/2.c8fb60cb.chunk.js"
+  },
+  {
+    "revision": "2eea0cf90c5c449b836f",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/js/main.0c489cb7.chunk.js"
+  },
+  {
+    "revision": "6b1b06103e7d0a89bc3b",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/js/runtime~main.4165aaa0.js"
+  },
+  {
+    "revision": "3f2a4daede60a2fd953c7dd31fa71972",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/burger.3f2a4dae.png"
+  },
+  {
+    "revision": "f5b6ed116c1a672edcfc8dd29bb7e69f",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/floor.f5b6ed11.png"
+  },
+  {
+    "revision": "65fd5b77ad79d908b38f1b913c392140",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/ground.65fd5b77.png"
+  },
+  {
+    "revision": "fc41d8c475c7cefe4e5e77b96a6a2e6f",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/houses.fc41d8c4.png"
+  },
+  {
+    "revision": "746323a4ffd8540c15beb78e0ff61eec",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/kitchen.746323a4.png"
+  },
+  {
+    "revision": "3f7c63c6eb55148311e9decba30db66f",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/picture.3f7c63c6.png"
+  },
+  {
+    "revision": "8af7211326cceb52ec43da113c9e988d",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/plate.8af72113.png"
+  },
+  {
+    "revision": "ab1e31dd399e53c8d035dc23fb34d878",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/room.ab1e31dd.png"
+  },
+  {
+    "revision": "c15ed25458fe27bae6e90bdb222360e0",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/room.c15ed254.png"
+  },
+  {
+    "revision": "cda649da851218c87e9595dead90e024",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/room.cda649da.png"
+  },
+  {
+    "revision": "08e91c434e24f6c3bd4c0a5e28756499",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/signboard.08e91c43.png"
+  },
+  {
+    "revision": "b17e6031047271a8bf57c6d4ea298ceb",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/table.b17e6031.png"
+  },
+  {
+    "revision": "aa4e1286ab429892c5fd0ec0b268dadb",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/winner.aa4e1286.png"
+  },
+  {
+    "revision": "6a9cdb6b1f93f38913529f0ee73d31f4",
+    "url": "https://sergeyprokopenko.github.io/csb-6m9ov/static/media/wooden-floor.6a9cdb6b.png"
+  }
+]);
